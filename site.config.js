e9dcd8f829ebfca75b8cbe1b02e95479d8bce870
J3K <ifirstwhy@gmail.com>
2024-08-08 12:24:20 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   profile: {
     name: "J3K",
     image: "/avatar.png", // 아바타 이미지 경로
-    role: "< VVorlcl />",
+    role: "< vvorlcl />",
     bio: "",
     email: "",
     linkedin: "",
